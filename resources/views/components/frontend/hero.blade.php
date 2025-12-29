8d9d7f4d1465dbe74f4445d51d01b448bc1fba0c
pentanikit <pentanikit2023@gmail.com>
2025-12-29 17:15:48 +0600

    new hero img

--- resources/views/components/frontend/hero.blade.php
+++ resources/views/components/frontend/hero.blade.php
@@ -1,5 +1,5 @@
 <section class="py-3">
-    <div class="container"  style="background-image: url({{ asset('Online-Market-BD-Banner-White-bg.jpg'); }})">
+    <div class="container"  style="background-image: url({{ asset('Online-Market-BD-Banner-White-bg.jpg') }})">
         <div class="hero-card">
             <div class="row align-items-center g-4">
                 <div class="col-md-6 text-center text-md-start">

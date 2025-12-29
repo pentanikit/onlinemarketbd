f7c53ae9e6e32bb5928527c9b2e3a640d01feebe
pentanikit <pentanikit2023@gmail.com>
2025-12-29 17:18:36 +0600

    old in

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -255,7 +255,7 @@
         <div class="container ">
             <div class="row align-items-center text-center text-md-start">
                 <!-- Left image circle -->
-                <div class="col-md-4 d-flex justify-content-center  mb-md-0">
+                <div class="col-md-6 d-flex justify-content-center  mb-md-0">
                     <div class="listing-photo-wrapper text-center">
                         <!-- replace this image with your own -->
                       
@@ -267,7 +267,7 @@
                 </div>
 
                 <!-- Right content -->
-                <div class="col-md-8 text-center">
+                <div class="col-md-6 text-center">
                     <h2 class="manage-title">Manage your <span>free</span> listing.</h2>
                     {{-- <h2 class="manage-title" style="font-size:32px; font-weight:700">
                         {{ $content->manage_title }}

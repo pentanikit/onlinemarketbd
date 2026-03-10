b694c18b8f9cb6251c979b605cdd196a20ab70c4
pentanikit <pentanikit2023@gmail.com>
2026-03-10 16:50:27 +0600

    fixed 2

--- app/Http/Controllers/SellerProductsController.php
+++ app/Http/Controllers/SellerProductsController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
-class ClassifiedAdController extends Controller
+class SellerProductsController extends Controller
 {
     public function index(Request $request)
     {
